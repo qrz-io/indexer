e7a32ee966dc97af782f9f8ad99f20399286a13d
Cristian Quiroz <cdquirozc@gmail.com>
2015-02-22 22:19:09 +0000

    Created indexer model + initial basic structure

---
+++ app/code/community/QuirozDev/Indexer/Model/Indexer.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Class QuirozDev_Indexer_Model_Indexer
+ * This class mimics functionality found in Mage_Shell_Compiler
+ */
+class QuirozDev_Indexer_Model_Indexer
+{
+    const XML_PATH_INDEX_INDEX_MODEL = 'global/index/index_model';
+
+    /**
+     * @param string $param
+     * @return array Array of messages with success/failure from reindex processes.
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function reindex($param = 'all')
+    {
+        $processes = $this->parseIndexerString($param);
+        $results = array(
+            'success' => array(),
+            'error'   => array(),
+        );
+
+        try {
+            foreach ($processes as $process) {
+                /* @var $process Mage_Index_Model_Process */
+                try {
+                    $process->reindexEverything();
+                    Mage::dispatchEvent($process->getIndexerCode() . '_shell_reindex_after');
+                    $results['success'][] = $process->getIndexer()->getName() . ' index was rebuilt successfully.';
+                } catch (Mage_Core_Exception $e) {
+                    $results['error'][] = $process->getIndexer()->getName() . ' index process error: ' . $e->getMessage();
+                } catch (Exception $e) {
+                    $results['error'][] = $process->getIndexer()->getName() . ' index process unknown error:' . $e->getMessage();
+                }
+            }
+            Mage::dispatchEvent('shell_reindex_finalize_process');
+        } catch (Exception $e) {
+            Mage::dispatchEvent('shell_reindex_finalize_process');
+            $results['error'][] = $e->getMessage();
+        }
+
+        return $results;
+    }
+
+    /**
+     * Parse string with indexers and return array of indexer instances
+     * @param string $string
+     * @return array
+     * @throws Exception
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function parseIndexerString($string = 'all')
+    {
+        $processes = array();
+        if ($string == 'all') {
+            $collection = $this->getIndexer()->getProcessesCollection();
+            echo get_class($collection) . '12';
+            foreach ($collection as $process) {
+                if ($process->getIndexer()->isVisible() === false) {
+                    continue;
+                }
+                $processes[] = $process;
+            }
+        } else {
+            if (!empty($string)) {
+                $codes = explode(',', $string);
+                $codes = array_map('trim', $codes);
+                $processes = $this->getIndexer()->getProcessesCollectionByCodes($codes);
+                foreach ($processes as $key => $process) {
+                    if ($process->getIndexer()->getVisibility() === false) {
+                        unset($processes[$key]);
+                    }
+                }
+                if ($this->getIndexer()->hasErrors()) {
+                    throw new Exception(implode(PHP_EOL, $this->getIndexer()->getErrors()), PHP_EOL);
+                }
+            }
+        }
+
+
+        return $processes;
+    }
+
+    /**
+     * @return mixed
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    public function getIndexProcessCollection()
+    {
+        $collection = Mage::getResourceModel('enterprise_index/process_collection');
+        Mage::app()->dispatchEvent(
+            'enterprise_index_exclude_process_before',
+            array('collection' => $collection)
+        );
+        $collection->initializeSelect();
+
+        return $collection;
+    }
+
+    /**
+     * Get Indexer instance
+     * @return Mage_Index_Model_Indexer
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    protected function getIndexer()
+    {
+        return Mage::getSingleton($this->getIndexClassAlias());
+    }
+
+    /**
+     * @return string
+     * @author Cristian Quiroz <cris@qcas.co>
+     */
+    protected function getIndexClassAlias()
+    {
+        return (string) Mage::getConfig()->getNode(self::XML_PATH_INDEX_INDEX_MODEL);
+    }
+}
